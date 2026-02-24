f61a70330324b9537299868127eef1b85a828bf8
Izaan Khan <izaanalikhan2008@gmail.com>
2026-02-24 14:12:39 +0530

    Update _main.py

--- Pyphy/v 1.0/_main.py
+++ Pyphy/v 1.0/_main.py
@@ -3,20 +3,21 @@ import math
 
 source = Screen(Vector2(800, 600), "Pyphy Test") # IMPORTANT
 
-source.scene.start()
 
-while source.running:
-            source.clear()
-            source.handle_events() 
+class Game(Scene):
 
-            source.scene.update(source.delta_time)
-            source.scene.physics_update(source.delta_time)
-            source.scene.render(source)   
+    def start(self):
+        self.tobj = StageObject() 
+        self.t = self.tobj.add_component(ShapeRenderer(Triangle(Vector2(100, 100)), RGBa.RED, RGBa.RED))
+        self.add_objects(self.tobj)
+        self.tobj.transform.position = self.screen.centre
 
-            # CODE
-
-            source.update()
-
+    def update(self, dt):
+        print("Hello world")
+        
+        
+source.set_scene(Game())
+source.run()
 
 
 
